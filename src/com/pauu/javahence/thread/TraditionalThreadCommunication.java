41091d5b3efae889b7f8b6c65a51d096f77021fe
peng.xing <southeast_px@163.com>
2016-08-18 09:46:57 +0800

    Thread Communication

---
+++ src/com/pauu/javahence/thread/TraditionalThreadCommunication.java
@@ -0,0 +1,51 @@
+package com.pauu.javahence.thread;
+
+public class TraditionalThreadCommunication {
+	final static Business business = new Business();
+	public static void main(String[] args) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for(int i=1;i<=50;i++){
+					business.sub(i);
+				}
+			}
+		}).start();
+		
+		for(int i=1;i<=50;i++){
+			business.main(i);
+		}
+	}
+}
+class Business{
+	private boolean bShouldSub = true;
+	public synchronized void sub(int i){
+		while(!bShouldSub){
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		for(int j=1;j<=10;j++){
+			System.out.println("sub thread squence of "+j+" loop of "+i);
+		}
+		bShouldSub = false;
+		this.notify();
+	}
+	
+	public synchronized void main(int i){
+		while(bShouldSub){
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		for(int j=1;j<=10;j++){
+			System.out.println("main thread squence of "+j+" loop of "+i);
+		}
+		bShouldSub = true;
+		this.notify();
+	}
+}
