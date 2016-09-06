6dffbd9fe485aed9bbb277aa974faa69a0112a9f
peng.xing <southeast_px@163.com>
2016-09-06 13:28:01 +0800

    jdk5新特性--静态导入

---
+++ src/com/pauu/javahence/jdk5/StaticImport.java
@@ -0,0 +1,14 @@
+package com.pauu.javahence.jdk5;
+
+import static java.lang.Math.*;
+/**
+ * 测试jdk5新特性，静态导入
+ * @author peng.xing
+ *
+ */
+public class StaticImport {
+	public static void main(String[] args) {
+		System.out.println(max(3, 6));
+		System.out.println(abs(3 - 6));
+	}
+}
