f3785371fbcdd866b3ba75ce5544133009deb13a
peng.xing <southeast_px@163.com>
2016-09-07 14:04:10 +0800

    反射Class类

---
+++ src/com/pauu/javahence/jdk5/ReflectionTest.java
@@ -0,0 +1,19 @@
+package com.pauu.javahence.jdk5;
+
+public class ReflectionTest {
+	public static void main(String[] args) throws Exception{
+		String str1 = "abc";
+		Class cls1 = str1.getClass();
+		Class cls2 = String.class;
+		Class cls3 = Class.forName("java.lang.String");
+		System.out.println(cls1==cls2);//true
+		System.out.println(cls1==cls3);//true
+		System.out.println(cls1.isPrimitive());//false,String不是基本类型
+		System.out.println(int.class.isPrimitive());//true,int是基本类型
+		System.out.println(Integer.class.isPrimitive());//false,Integer不是基本类型
+		System.out.println(int.class == Integer.class);//false
+		System.out.println(int.class == Integer.TYPE);//true
+		System.out.println(int[].class.isPrimitive());//false,数组不是基本类型
+		System.out.println(int[].class.isArray());//true
+	}
+}
