2c562f5a91907acd677cf4bdfeacba987f32b588
peng.xing <southeast_px@163.com>
2016-09-09 15:54:05 +0800

    数组反射

--- src/com/pauu/javahence/jdk5/ReflectionTest.java
+++ src/com/pauu/javahence/jdk5/ReflectionTest.java
@@ -3,6 +3,7 @@ package com.pauu.javahence.jdk5;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class ReflectionTest {
 	public static void main(String[] args) throws Exception{
@@ -47,6 +48,24 @@ public class ReflectionTest {
 		mainMethod.invoke(null, new Object[]{new String[]{"111","222","333"}});
 		mainMethod.invoke(null, (Object)new String[]{"111","222","333"});
 		
+		//数组，只有当类型相同，并且维度相同时，才是同一份字节码
+		int[] a1 = new int[3];
+		int[] a2 = new int[4];
+		int[][] a3 = new int[2][3];
+		String[] a4 = new String[3];
+		System.out.println(a1.getClass() == a2.getClass());//true
+		//System.out.println(a1.getClass() == a3.getClass());//false
+		//System.out.println(a1.getClass() == a4.getClass());//false
+		System.out.println(a1.getClass().getName());//[I
+		System.out.println(a1.getClass().getSuperclass().getName());//java.lang.Object
+		System.out.println(a3.getClass().getSuperclass().getName());//java.lang.Object
+		
+		int[] a5 = new int[]{1,2,3};
+		String[] a6 = new String[]{"a","b","c"};
+		Integer[] a7 = new Integer[]{4,5,6};
+		System.out.println(Arrays.asList(a5));//[[I@18a49e0]
+		System.out.println(Arrays.asList(a6));//[a, b, c]
+		System.out.println(Arrays.asList(a7));//[4, 5, 6]
 	}
 
 	private static void changeStringValue(Object point) throws Exception {
