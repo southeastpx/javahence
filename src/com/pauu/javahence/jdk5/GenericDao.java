0db63541077cb3b8ef1e5844461d8baee272d19b
peng.xing <southeast_px@163.com>
2016-10-13 10:36:44 +0800

    dao层使用泛型

---
+++ src/com/pauu/javahence/jdk5/GenericDao.java
@@ -0,0 +1,34 @@
+package com.pauu.javahence.jdk5;
+
+import java.util.Set;
+/**
+ * Dao≤„ π”√∑∫–Õ
+ * @author peng.xing
+ *
+ * @param <T>
+ */
+public class GenericDao<T> {
+	public void add(T obj){
+		
+	}
+	
+	public T findById(int id){
+		return null;
+	}
+	 
+	public void delete(T obj){
+		
+	}
+	 
+	public void delete(int id){
+		
+	}
+	
+	public void update(T obj){
+		
+	}
+	
+	public Set<T> findByConditions(String where){
+		return null;
+	}
+}
